7da3f5712ccb4cb6537d665f2d658cf291f995e9
deliciads <delicia1999@yahoo.com>
2022-09-30 18:06:49 -0400

    Css and content added

--- routes/index.js
+++ routes/index.js
@@ -21,55 +21,17 @@ router.get('/about', function(req, res, next) {
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects'});
+  res.render('projects', { title: 'Projects'});
 });
 
 /* GET services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services'});
+  res.render('services', { title: 'Services'});
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact'});
 });
-
-router.get("/process_get", function (req, res, next) {
-  response = {
-    name: req.query.name,
-    lname: req.query.lname,
-    email: req.query.email,
-    number: req.query.number,
-    message: req.query.message,
-  };
-  console.log(response);
-  res.redirect('/');
-});
-
-/*app.get("/process_get", function (req, res) {
-  response = {
-    name: req.query.name,
-    lname: req.query.lname,
-    //email: req.query.email,
-    //number: req.query.number,
-    //message: req.query.message,
-  };
-  console.log(response);
-  res.end(JSON.stringify(response));
-});*/
-
-//pdf function
-/*app.get('/', function(req, res) {
-  res.sendFile(__dirname + "/public/pdf/resume.pdf");
-})*/
-
-/*app.get('/pdf', function (req, res) {
-  var filePath = "/public/pdf/resume.pdf";
-
-  fs.readFile(__dirname +filePath, function (err,data) {
-    res.contentType("application/pdf");
-    res.send(data);
-  });
-});*/
 
 module.exports = router;
